4996f11ec8aea7b126ca7914d6c863a0591f3173
rjvshyam <158989930+rjvshyam@users.noreply.github.com>
2024-02-19 19:10:49 +0530

    Update Palindrome.java

--- HA/Palindrome.java
+++ HA/Palindrome.java
@@ -3,16 +3,16 @@ package week1.HA;
 public class Palindrome {
 
 public static void main(String[] args) {
-int input = 121; int output = 0;
-
-for (int i=input; i>0; i/=10) {
-int rem = i%10;
-output= rem;
-if (input==rem) {
-System.out.println("Number is palindrome:"+rem);
-} else
-{System.out.println("Number is not palindrome:"+rem);
+int num = 121, sum, temp, digit;
+temp=num;
+for (sum=0; temp>0; temp/=10) {
+	digit = temp % 10;
+	sum=(sum*10)+digit;
 }
+if (sum==num) {
+System.out.println("Number is palindrome:"+num);
+} else
+{System.out.println("Number is not palindrome:"+num);
 }
 
 }
