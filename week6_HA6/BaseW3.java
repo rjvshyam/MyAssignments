0bbd67a5a1a792e10b2ffd1e361a69dc9fb9efd1
rjvshyam <158989930+rjvshyam@users.noreply.github.com>
2024-03-23 10:36:17 +0530

    Update BaseW3.java

--- week6_HA6/BaseW3.java
+++ week6_HA6/BaseW3.java
@@ -30,3 +30,18 @@ public void postcondW3() {
 	driver.close();
 	}
 }
+
+***********************************************************************************************************************
+	<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+<suite name="Suite" verbose="5">
+<parameter name="url" value="https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_checkout_form"></parameter>
+<parameter name="browser" value="chrome"></parameter>
+  <test name="Test">
+
+    <classes>
+      <class name="week6_HA6.registrationW3"/>
+    </classes>
+  </test> <!-- Test -->
+</suite> <!-- Suite -->
+
